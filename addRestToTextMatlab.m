068fc1d1e64406efef4b33d55a96c5716ec6a742
Erik Arnold <erikarnold@umass.edu>
2018-07-12 13:51:54 -0400

    Trying to work around the EVT writing

--- addRestToTextMatlab.m
+++ addRestToTextMatlab.m
@@ -4,32 +4,41 @@
 function addRest()
   %Set path to folder on your current computer
 %   if isempty(fPath)
-    fPath =  '/Users/user/Downloads/Summer Training';
+  fPath =  '/Users/user/Downloads/Summer Training';
 %   end
-  restTrig = '0 1 1 0 0 0 0 0';
+  restTrig = '0 0 1 1 0 0 0 0 0';
   %finds all the .evt file locations
-  evtFilePath = rdir(fullfile(fPath,'*/','**/','*/','*.evt'))
+  evtFilePath = rdir(fullfile(fPath,'*/','**/','*/','*.evt'));
   %Only keeps the path to the evt files
   evtFilePath = {evtFilePath(:).name};
   %cycle through the EVT files checking for rest conditions
   for n = 1:length(evtFilePath)
+    disp(n)  
+    disp(length(evtFilePath))
     curEVT = load(char(evtFilePath(n)));
-    timeIdx = curEVT(:,1);
+    oGEVT = load(char(evtFilePath(n)));
+    disp(char(evtFilePath(n)))
+%     timeIdx = curEVT(:,1);
     %get the first binary event code
     fstEVT = curEVT(1,:);
     %1 is the time Frame
     fstEVT = strcat(num2str(fstEVT(2)),num2str(fstEVT(3)),num2str(fstEVT(4)),num2str(fstEVT(5)));
     %if there is no rest event at the begining
     if strcmp(fstEVT,'0110')
-      break
+        disp ('there is a rest')    
     else
       %print the output to a text file with the rest condition first (CURRENTLY STARTING AT 0)
+      disp('There is no rest')
       fstRest = sprintf('0 %s',restTrig);
-      curEVTfile = fopen(char(evtFilePath(n)),'w');
-      fprintf(curEVTfile,'%s\n',fstRest)
+%      curEVTfile = fopen(char(evtFilePath(n)),'w');
+%      dlmwrite(char(curEVTfile),fstRest)
       %% KNOWN BUG, makes output into one HUGE number insted of the format prefed by anyone and everyone
-      fprintf(curEVTfile,'%d %i %i %i %i %i %i %i %i', curEVT)
+%      dlmwrite(char(curEVTfile),oGEVT,'-append',' ')
+      comboEVT = [restTrig; oGEVT;]
+      disp(evtFilePath(n))
+      writetable(comboEVT,char(evtFilePath(n)))
     end
+    
   end
 
       %put in first rest code
