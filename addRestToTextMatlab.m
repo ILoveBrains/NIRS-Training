bca33b7f75c71d3e843c19416d5fbd0dab6b21ed
Erik Arnold <erikarnold@umass.edu>
2018-09-26 11:28:17 -0400

    Cleaned a bit

--- addRestToTextMatlab.m
+++ addRestToTextMatlab.m
@@ -3,7 +3,6 @@
 %% DEPENDENCY ON rdir, find_and_replace
 function addRest()
   %Set path to folder on your current computer
-%   if isempty(fPath)
   curDir = cd;
   fPath =  '/Users/erikarnold/Downloads';
 %   end
@@ -34,33 +33,12 @@ function addRest()
       curEVTf = char(evtFilePath(n));
       [~,name,ext] = fileparts(char(evtFilePath(n)));
       fSavName = strcat(name,ext);
-%      dlmwrite(char(curEVTfile),fstRest)
-      %% KNOWN BUG, makes output into one HUGE number. This is not the format prefed by anyone and everyone
-%      dlmwrite(char(curEVTfile),oGEVT,'-append',' ')
-%       comboEVT = array2table(vertcat(restTrig, oGEVT));
-%       disp(evtFilePath(n))
-%       curTableName = sprintf('eventfile%i.csv',n);
-%       writetable(comboEVT,curTableName)
-%       movefile(curTableName,char(evtFilePath(n)))
-%       %%
-%       fgetl(curEVTf) ;                                  % Read/discard line.
-%       buffer = fread(curEVTf, Inf) ;                    % Read rest of the file.
-%       fwrite(curEVTf, buffer) ;                         % Save to file.
-%       fclose(curEVTf) ;
-%       %%
-%       % make the spacing something more like the original code
-%       find_and_replace(evtFilePath,',',' ')
-%       find_and_replace(evtFilePath,'    ',' ')
-        curEVTf = load(curEVTf);
-        comboEVT = vertcat(restTrig,curEVTf);
-        dlmwrite(fSavName,comboEVT,' ');
-        fileMoveName = strcat(name,ext);
-        movefile(fileMoveName,fileparts(char(evtFilePath(n))))
+      curEVTf = load(curEVTf);
+      comboEVT = vertcat(restTrig,curEVTf);
+      dlmwrite(fSavName,comboEVT,' ');
+      fileMoveName = strcat(name,ext);
+      %Move the file back to the original EVT file path
+      movefile(fileMoveName,fileparts(char(evtFilePath(n))))
     end
 
   end
-
-      %put in first rest code
-    %% What was the timing we decided to add?
-  %a = sprintf('%d %d %d', 0)
-    %fprintf the rest we desided on, then the rest of the file after.
